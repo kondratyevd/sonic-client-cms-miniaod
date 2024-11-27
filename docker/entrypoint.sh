6f6424eff76d3e29fb7fe251388fa899d3b0f04c
kondratyevd <kondratyev.d.95@gmail.com>
2024-11-27 18:53:52 -0500

    debug

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,10 +5,8 @@ set -e
 cd /home/CMSSW_14_1_0_pre7/src/sonic-workflows
 
 source /cvmfs/cms.cern.ch/cmsset_default.sh
-cmsenv
-export CMS_PATH=/etc/
+export CMS_PATH=/cvmfs/cms-ib.cern.ch/
 
-env | grep PATH
-env | grep cvmfs
+cmsenv
 
 cmsRun run.py "$@"
